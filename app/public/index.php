9bfe84703eec9e12d31d2fb9c49d99396a8b95d5
Wandonium <hillarywando@gmail.com>
2023-07-25 21:20:50 +0300

    Fixed wrong db credentials causing PDO error.

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:dbname=my_db;host=mysql', 'root', 'root@123', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+$pdo = new PDO('mysql:dbname=my_db;host=mysql', 'db_user', 'db_user@123', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 $query = $pdo->query('SHOW VARIABLES like "version"');
 
